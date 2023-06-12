06d9207b9d4cb9de5ecdf6be1d871ecbb00356d0
mmask <mmask@Roshan>
2023-06-12 17:24:37 +0530

    5th commit

---
+++ src/test/java/category_Module/Prakashsa.java
@@ -0,0 +1,16 @@
+package category_Module;
+
+public class Prakashsa {
+
+	public static void main(String[] args) {
+		System.out.println("rama");
+		System.out.println("lakshaman");
+
+		System.out.println("sita");
+
+		System.out.println("hanumana");
+
+
+	}
+
+}
