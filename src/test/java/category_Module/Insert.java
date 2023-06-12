efbfb7a4132d0a434c05dacb2f8a7e513ae5fc03
mmask <mmask@Roshan>
2023-06-12 15:48:04 +0530

    Roshan 1st commit

---
+++ src/test/java/category_Module/Insert.java
@@ -0,0 +1,13 @@
+package category_Module;
+
+import org.testng.annotations.Test;
+
+public class Insert {
+	@Test
+	public void sampleTest1()
+	{
+		System.out.println("Testcase1 Roshan");
+	}
+	
+
+}
